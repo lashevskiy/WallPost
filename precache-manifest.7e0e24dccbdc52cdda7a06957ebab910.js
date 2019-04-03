d211406b6f418b4e4190a7b07229471bfb87be99
lashevskiy <lashevskiy@gmail.com>
2019-04-03 21:26:05 +0300

    Updates

---
+++ precache-manifest.7e0e24dccbdc52cdda7a06957ebab910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/WallPost/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "d1561c7c7d5890f4412f",
+    "url": "/WallPost/static/js/runtime~main.b5f2a57b.js"
+  },
+  {
+    "revision": "917489bb47d806a346a3",
+    "url": "/WallPost/static/js/main.cdce495f.chunk.js"
+  },
+  {
+    "revision": "f2071c56116ccd58534f",
+    "url": "/WallPost/static/js/2.8b4b34f6.chunk.js"
+  },
+  {
+    "revision": "917489bb47d806a346a3",
+    "url": "/WallPost/static/css/main.f640144b.chunk.css"
+  },
+  {
+    "revision": "f2071c56116ccd58534f",
+    "url": "/WallPost/static/css/2.e785127d.chunk.css"
+  },
+  {
+    "revision": "14b1e4a44ce8198d7faa135c1305ada4",
+    "url": "/WallPost/index.html"
+  }
+];
