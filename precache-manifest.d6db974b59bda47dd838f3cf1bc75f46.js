a7ea01e224c32643f7ae973791cd87ddf7c62350
lashevskiy <lashevskiy@gmail.com>
2019-04-04 23:06:51 +0300

    Updates

--- precache-manifest.d6db974b59bda47dd838f3cf1bc75f46.js
+++ precache-manifest.d6db974b59bda47dd838f3cf1bc75f46.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/WallPost/static/js/runtime~main.b5f2a57b.js"
   },
   {
-    "revision": "1461fec4f66333ca1915",
-    "url": "/WallPost/static/js/main.048d3c68.chunk.js"
+    "revision": "b5b28740db5d234c04f5",
+    "url": "/WallPost/static/js/main.a661e680.chunk.js"
   },
   {
     "revision": "33b7f40b29d15c9b8a43",
     "url": "/WallPost/static/js/2.e431db31.chunk.js"
   },
   {
-    "revision": "1461fec4f66333ca1915",
+    "revision": "b5b28740db5d234c04f5",
     "url": "/WallPost/static/css/main.f640144b.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/WallPost/static/css/2.e785127d.chunk.css"
   },
   {
-    "revision": "6be748b7d38b8e04694fd5cb45f58e45",
+    "revision": "192c5750392ef53a5bdc4530b8fd2ab0",
     "url": "/WallPost/index.html"
   }
 ];
